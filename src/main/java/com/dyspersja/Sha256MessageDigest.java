58cb18062f63556afa74efc30998b590681b7a13
Dyspersja <dyspersjamusic@gmail.com>
2024-01-11 19:34:15 +0100

    Added method that computes final SHA256 hash value

--- src/main/java/com/dyspersja/Sha256MessageDigest.java
+++ src/main/java/com/dyspersja/Sha256MessageDigest.java
@@ -2,8 +2,37 @@ package com.dyspersja;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Sha256MessageDigest {
+
+    // The K constants
+    // 64 32-bit values obtained by taking the first 32 bits of the
+    // fractional parts of the cube roots of the first 64 primes
+    private final int[] K = {
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
+
+    // Initial Hash values
+    // 8 32-bit values obtained by taking the first 32 bits of the
+    // factorial parts of the square roots of the first 8 prime numbers
+    private final int[] H0 = {
+            0x6a09e667,
+            0xbb67ae85,
+            0x3c6ef372,
+            0xa54ff53a,
+            0x510e527f,
+            0x9b05688c,
+            0x1f83d9ab,
+            0x5be0cd19
+    };
 
     public byte[] digest(String input) {
         // Step 1: Encode the input to binary using UTF-8
@@ -25,8 +54,8 @@ public class Sha256MessageDigest {
         int[][] messageSchedules = createMessageSchedules(chunks);
         // Compute words from 16th to 64th in each message schedule
         int[][] expandedMessageSchedules = computeMessageSchedules(messageSchedules);
-
-        return null;
+        // Calculate the final 256-bit hash value
+        return calculateHash(expandedMessageSchedules);
     }
 
     private byte[] createMessageBlock(byte[] sourceBytes) {
@@ -134,5 +163,78 @@ public class Sha256MessageDigest {
         return Integer.rotateRight(x,17) ^
                 Integer.rotateRight(x,19) ^
                 (x >>> 10);
+    }
+
+    private byte[] calculateHash(int[][] expandedMessageSchedules) {
+        // Copy initial Hash values to new array
+        int[] H = Arrays.copyOf(H0, H0.length);
+
+        // Perform following computations sequentially for every expanded message schedule
+        for(int[] expandedMessageSchedule : expandedMessageSchedules) {
+            // Step 9: Initialize registers a, b, c, d, e, f, g, h with hash values
+            // calculated from the previous message scheduler, H0 for first one
+            int a = H[0], b = H[1], c = H[2], d = H[3],
+                e = H[4], f = H[5], g = H[6], h = H[7];
+
+            // Step 10: For every word in a message scheduler perform following operations:
+            for (int i = 0 ;i < 64; i++) {
+                int T1 = h + bigSigma1(e) + ch(e,f,g) + K[i] + expandedMessageSchedule[i];
+                int T2 = bigSigma0(a) + maj(a,b,c);
+                h = g; g = f; f = e; e = d + T1; d = c; c=b; b=a; a=T1+T2;
+            }
+
+            // Step 11: Compute and save hash value for current message scheduler
+            H[0] += a; H[1] += b; H[2] += c; H[3] += d;
+            H[4] += e; H[5] += f; H[6] += g; H[7] += h;
+        }
+
+        // Step 12: After all message schedulers have been computed the final SHA-256
+        // hash value is the concatenation of the values from the H array.
+        ByteBuffer byteBuffer = ByteBuffer.allocate((256/8));
+        for (int value : H) byteBuffer.putInt(value);
+
+        return byteBuffer.array();
+    }
+
+    private int bigSigma0(int x) {
+        // Simple function that uses bitwise operations
+        //
+        // Rotate the bits to the right by 2 positions
+        // Rotate the bits to the right by 13 positions
+        // Rotate the bits to the right by 22 positions
+        //
+        // Apply bitwise XOR to the rotated and shifted values
+        return Integer.rotateRight(x,2) ^
+                Integer.rotateRight(x,13) ^
+                Integer.rotateRight(x,22);
+    }
+
+    private int bigSigma1(int x) {
+        // Simple function that uses bitwise operations
+        //
+        // Rotate the bits to the right by 6 positions
+        // Rotate the bits to the right by 11 positions
+        // Rotate the bits to the right by 25 positions
+        //
+        // Apply bitwise XOR to the rotated and shifted values
+        return Integer.rotateRight(x,6) ^
+                Integer.rotateRight(x,11) ^
+                Integer.rotateRight(x,25);
+    }
+
+    private int ch(int e, int f, int g) {
+        // Function "Choose"
+        // Chooses a bit from values f or g based on the corresponding bit from value e
+        // If the bit in value e is '0', the result bit is taken from value g if '1',
+        // it is taken from value f
+        return (e & f) ^ ((~e) & g);
+    }
+
+    private int maj(int a, int b, int c) {
+        // Function "Majority"
+        // Compares corresponding bits in values a, b, and c
+        // If there are more '0' bits than '1' bits, the result bit is set to '0'
+        // otherwise, it is set to '1'
+        return (a & b) ^ (a & c) ^ (b & c);
     }
 }

90443df53b4f94e1d2094db2e334a73472b3fe6a
Dyspersja <dyspersjamusic@gmail.com>
2024-01-11 15:06:26 +0100

    Added method digest for getting hash out of String input and implemented first step of algorithm

--- src/main/java/com/dyspersja/Sha256MessageDigest.java
+++ src/main/java/com/dyspersja/Sha256MessageDigest.java
@@ -1,4 +1,13 @@
 package com.dyspersja;
 
+import java.nio.charset.StandardCharsets;
+
 public class Sha256MessageDigest {
+
+    public byte[] digest(String input) {
+        // Step 1: Encode the input to binary using UTF-8
+        byte[] sourceBytes = input.getBytes(StandardCharsets.UTF_8);
+        
+        return null;
+    }
 }

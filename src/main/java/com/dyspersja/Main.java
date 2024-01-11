8108ab40531688bd9a768109d56e89830a50155e
Dyspersja <dyspersjamusic@gmail.com>
2024-01-11 20:19:50 +0100

    Added code to main method to demonstrate the program's functionality

--- src/main/java/com/dyspersja/Main.java
+++ src/main/java/com/dyspersja/Main.java
@@ -1,7 +1,20 @@
 package com.dyspersja;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        // Example with byte[] input (e.g., file content)
+//        byte[] sourceData = Files.readAllBytes(Paths.get("D:\\file"));
+
+        // Example with String input
+        String sourceData = "Hello World!";
+
+        Sha256MessageDigest messageDigest = new Sha256MessageDigest();
+
+        byte[] hash = messageDigest.digest(sourceData);
+        for (byte b : hash) System.out.printf("%02x", b & 0xFF);
     }
 }

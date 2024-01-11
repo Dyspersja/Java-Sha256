7421c45a6a08e383590184ba480e6d221938d1d2
Dyspersja <dyspersjamusic@gmail.com>
2024-01-11 15:22:17 +0100

    Added digest method overload to be able to compute sha256 hash from file bytes

--- src/main/java/com/dyspersja/Sha256MessageDigest.java
+++ src/main/java/com/dyspersja/Sha256MessageDigest.java
@@ -8,6 +8,14 @@ public class Sha256MessageDigest {
         // Step 1: Encode the input to binary using UTF-8
         byte[] sourceBytes = input.getBytes(StandardCharsets.UTF_8);
         
+        return generate(sourceBytes);
+    }
+
+    public byte[] digest(byte[] sourceBytes) {
+        return generate(sourceBytes);
+    }
+
+    private byte[] generate(byte[] sourceBytes) {
         return null;
     }
 }
